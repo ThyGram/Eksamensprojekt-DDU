b9379f684db911f00da411afcc5da945ec05caa0
Alvar Vähälummukka <alvar@lummukka.com>
2025-04-09 11:40:00 +0200

    Semi-functional calendar

--- GodotGame/MainGame/Computer/computer_calender.gd
+++ GodotGame/MainGame/Computer/computer_calender.gd
@@ -1,9 +1,35 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var circle_shader = preload("res://Shaders/circle.gdshader")
+	var shader_material = ShaderMaterial.new()
+	shader_material.shader = circle_shader
+	
+	#Shader parametre indstilles for cirklen.
+	shader_material.set_shader_parameter("inner_color", Color(1,1,1,0.0)) # Gennemsigtig
+	shader_material.set_shader_parameter("border_color", Color.hex(0x1f1f1fff)) # Samme farve som kryds
+	shader_material.set_shader_parameter("border_width", 0.075)
+	
+	# Sort Ring loades ind med rigtige shader indstillinger
+	var black_ring := Sprite2D.new()
+	black_ring.texture = preload("res://textures/circle.png")
+	black_ring.material = shader_material
+	
+	# Størrelse indstilles
+	#var size = black_ring.texture.get_size()
+	black_ring.scale = Vector2(200,200)
+	
+	for n in storage.Day:
+		var current_date = "Dates/" + str(n+1)
+		if n == storage.Day-1:
+			# Position indstilles alt efter dato
+			black_ring.position = get_node(current_date).get_global_position()
+			add_child(black_ring)
+		else:
+			var current_cross = current_date + "/Cross";
+			get_node(current_cross).visible = true;
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
