19a9870b56c703bb82e5dbcbed1c5ab2dc5ae151
Alvar Vähälummukka <alvar@lummukka.com>
2025-04-10 13:38:45 +0200

    WILLIAAAAAAAM

--- GodotGame/MainGame/main_game_doorchat.gd
+++ GodotGame/MainGame/main_game_doorchat.gd
@@ -1,28 +1,33 @@
 extends Control
 
-var Dialouge1 : Array = ["Hello. I am Lånehajen Haj and i seee you havent paid back your debt"
-, "I DEMAND THAT YOU WORK FOR 10 DAYS, AND GIVE ME ALL THE MONEY TO CLEAR YOUR DEBT!!!"]
+var Dialogue1 : Array = ["*KNOCK KNOCK KNOCK*", "Hello, I am Lånehajen Haj and I see you havent paid back your debt.."
+, "I DEMAND THAT YOU WORK FOR 10 DAYS, weekends off of course, AND GIVE ME ALL THE MONEY YOUR EARN TO CLEAR YOUR DEBT!!!"]
 
-var Dialouge2 : Array = ["EEEE", "FWECW"]
+var Dialogue2 : Array = ["*KNOCK KNOCK*", "Hello again. I came to remind you that you better pay me back.. or else..", "I EXPECT AT LEAST A BILLION GAZILLION DOLLARS!!!", "MUAHAHAHHA!"]
+
+var Dialogue3 : Array = []
 
 var stop : bool = false
-var currDialouge : Array = []
+var currDialogue : Array = []
 var timer = Timer.new()
 
 func _ready():
 	if storage.Day == 1:
-		$Name/Dialouge.text = Dialouge1[0]
-		currDialouge = Dialouge1
+		$Panel/Name/Dialogue.text = Dialogue1[0]
+		currDialogue = Dialogue1
 	elif storage.Day == 5:
-		$Name/Dialouge.text = Dialouge2[0]
-		currDialouge = Dialouge2
+		$Panel/Name/Dialogue.text = Dialogue2[0]
+		currDialogue = Dialogue2
+	elif storage.Day == 10:
+		$Panel/Name/Dialogue.text = Dialogue3[0]
+		currDialogue = Dialogue3
 		
 
 func _input(event):
 	if (event.is_action_pressed("LeftClick") and !stop):
-		var currIndex = currDialouge.find($Name/Dialouge.text, 0)
-		if (currIndex < currDialouge.size() - 1):
-			$Name/Dialouge.text = currDialouge[currIndex+1]
+		var currIndex = currDialogue.find($Panel/Name/Dialogue.text, 0)
+		if (currIndex < currDialogue.size() - 1):
+			$Panel/Name/Dialogue.text = currDialogue[currIndex+1]
 		else:
 			$ExitTimer.start()
 			stop = true
