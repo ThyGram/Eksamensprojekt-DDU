171991d22bb03b0d7231c33851781ddf6d8f134c
Alvar Vähälummukka <alvar@lummukka.com>
2025-04-30 11:05:19 +0200

    Investments and bank balance are evaluated at game end

--- GodotGame/MainGame/main_game_doorchat.gd
+++ GodotGame/MainGame/main_game_doorchat.gd
@@ -5,14 +5,16 @@ extends Control
 var Dialogue1 : Array = ["*KNOCK KNOCK KNOCK*", "Hello, I am Lånehajen Haj and I see you haven't paid back your debt.."
 , "I DEMAND THAT YOU WORK FOR 14 DAYS, weekends off of course, AND GIVE ME ALL THE MONEY YOUR EARN TO CLEAR YOUR DEBT!!!", "*Leaves*"]
 
-var Dialogue2 : Array = ["*KNOCK KNOCK*", "Hello again. I came to remind you that you better pay me back by Saturday the 13th.. or else..", "I EXPECT AT LEAST 10.000$ DOLLARS!!!", "MUAHAHAHHA!", "*Leaves*"]
+var Dialogue2 : Array = ["*KNOCK KNOCK*", "Hello again. I came to remind you that you better pay me back by Saturday the 13th.. or else..", "I EXPECT AT LEAST 1.000.000$ DOLLARS!!!", "MUAHAHAHHA!", "*Leaves*"]
 
 var Dialogue3Win : Array = ["*CRASH*", "HAND OVER THE MONEY, NOW!!", "You've made quite a bit of money I must say.", "This will suffice for now.", "If you're ever in need of some quick cash you can come take a loan at my shop.", "MUAHAHAHAAA!", "*Leaves*"]
-var Dialogue3Loss : Array = ["*CRASH*", "HAND OVER THE MONEY, NOW!!", "OH.", "This isn't nearly enough.", "*BANG BANG BANG*", "You did not pay back your debt in time..."]
+var Dialogue3Loss : Array = ["*CRASH*", "HAND OVER THE MONEY, NOW!!", "OH.", "This isn't nearly enough.", "*CLICK BANG BANG BANG*", "You did not pay back your debt in time..."]
 
 var stop : bool = false
 var currDialogue : Array = []
 var timer = Timer.new()
+
+var investmentMoney : int = 0
 
 func _ready():
 	if storage.Day == 1:
@@ -23,7 +25,9 @@ func _ready():
 		currDialogue = Dialogue2
 		storage.SharkTalk5 = true
 	elif storage.Day == 11:
-		if storage.Money > 10000:
+		for key in storage.Stocks:
+			investmentMoney += storage.Stocks[key][0]
+		if (storage.Money + storage.BankMoney + investmentMoney) > 1000000:
 			$Panel/Dialogue.text = Dialogue3Win[0]
 			currDialogue = Dialogue3Win
 		elif storage.Money <= 10000:
