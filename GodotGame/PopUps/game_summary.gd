3c57de5b2809144df440df7e17aa5ffd5d33ea09
ThyGram <Williamgram@outlook.dk>
2025-04-30 09:46:10 +0200

    game_summary fixed

--- GodotGame/PopUps/game_summary.gd
+++ GodotGame/PopUps/game_summary.gd
@@ -42,6 +42,7 @@ func _on_continue_pressed():
 
 
 func _on_quit_pressed():
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Menu/main_menu.tscn")
 
 var http_request : HTTPRequest = HTTPRequest.new()
